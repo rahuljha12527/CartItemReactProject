8e5b985ff3483408c49af853f67d83b3dd0dd8f6
rahuljha661 <rahuljhamansapur@gmail.com>
2020-04-13 15:03:15 +0530

    add props

--- src/Cart.js
+++ src/Cart.js
@@ -1,12 +1,63 @@
 import React from 'react';
 import CartItem from './CartItem';
 class Cart extends React.Component{
+
+    constructor(){
+        super();
+        this.state={
+            products:[
+                {
+                    price:999,
+                    title:'Watch',
+                    qty:1,
+                    img:'',
+                    id:1
+                },
+                {
+                    price:999,
+                    title:'Mobile Phone',
+                    qty:10,
+                    img:'',
+                    id:2
+                },
+                {
+                    price:39990,
+                    title:'Laptop',
+                    qty:4,
+                    img:'',
+                    id:3
+                },
+                {
+                    price:100,
+                    title:'Glasses',
+                    qty:20,
+                    img:'',
+                    id:4
+                }
+    
+    
+    
+        
+            ]
+    
+        }
+           }
+
     render(){
+        
+        const {products}=this.state;
+     
         return (
            <div className="cart">
-                <CartItem/>
-                <CartItem/>
-                <CartItem/>
+                {products.map((product)=>{
+                    return( 
+                    <CartItem product={product} 
+                    key={product.id}
+                          />
+                 )           
+                        
+                })}
+                 
            </div>
         );
     }      
@@ -14,4 +65,4 @@ class Cart extends React.Component{
 
 
 
-export default Cart ;
+export default Cart ;   
--- src/CartItem.js
+++ src/CartItem.js
@@ -1,16 +1,6 @@
 import React from 'react';
 
 class CartItem extends React.Component{
-        constructor(){
-            super();
-            this.state={
-                price:999,
-                title:'Mobile Phone',
-                qty:1,
-                img:''
-            }
-            this.testing();
-        }
 
         testing(){
             const promise=new Promise((resolve,reject)=>{
@@ -68,11 +58,12 @@ class CartItem extends React.Component{
             });
         }
     render (){
-        
-        console.log('render');
-        const {price,title,qty}=this.state;
+         
+        console.log('this.props',this.props);
+        const {price,title,qty}=this.props.product;
         return (
             <div className="cart-item">
+                {this.props.jsx}
                <div className="left-block">
                 <img style={styles.image}/>   
                 </div> 
